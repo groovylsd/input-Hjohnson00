158e18a35b2a4524cca7056a1a11c8bcbc15366c
guerrjos <guerrjos@011-4303-S12QD.students.cisd.conroeisd.loc>
2018-10-19 08:01:23 -0500

    added a print statement to show my change.

--- Input/src/input/Input.java
+++ Input/src/input/Input.java
@@ -35,6 +35,8 @@ public class Input {
           
           out.printf("%s %s %s %s %s", stringArr[0], stringArr[1], stringArr[2], stringArr[3], stringArr[4]);
           
+          System.out.println("dsd");
+          
           
           
           
